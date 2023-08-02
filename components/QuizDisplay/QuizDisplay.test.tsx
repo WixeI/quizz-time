72cf1de3445465f1c2106fa5e9ceb20abb80b9b0
Alan <Alanpereira2@hotmail.com>
2023-08-01 22:04:49 -0300

    te-odeio

--- components/QuizDisplay/QuizDisplay.test.tsx
+++ components/QuizDisplay/QuizDisplay.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { render, screen,  } from '@testing-library/react'
-import { beforeEach } from "node:test";
+import { render, screen, } from '@testing-library/react'
+import { beforeEach } from 'node:test';
 import QuizDisplay from './QuizDisplay';
 
 describe('Display Quiz Component', () => {
   beforeEach(() => {
     render(<QuizDisplay />);
+  });
+
+  it('View total numbers', () => {
+    const totalView = screen.getByTestId('quiz-display-total-view');
+
+    expect(totalView).toBeInTheDocument();
+  });
+
+  test('View Actual number', () => {
+    expect(screen.getByTestId('quiz-display-actual-view')).toBeInTheDocument();
   });
 });
--- components/QuizDisplay/QuizDisplay.tsx
+++ components/QuizDisplay/QuizDisplay.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function QuizDisplay() {
-  return <div>Railou</div>
+  return <div data-testId="quiz-display-total-view">Railou</div>
 }
 
 export default QuizDisplay;
